6ebe0b36d507ccd8a7b26ed74145c7854b1187f9
Erkki Heino <erkki.heino@aalto.fi>
2016-03-09 15:58:03 +0200

    Build

--- dist/sparql-service.js
+++ dist/sparql-service.js
@@ -1,3 +1,43 @@
+(function() {
+    'use strict';
+
+    angular.module('sparql', []);
+})();
+
+/*
+* Service for querying a SPARQL endpoint.
+* Takes the endpoint URL as a parameter.
+*/
+(function() {
+    'use strict';
+
+    /* eslint-disable angular/no-service-method */
+    angular.module('sparql')
+
+    .factory('SparqlService', SparqlService);
+
+    /* ngInject */
+    function SparqlService($http, $q) {
+        return function(endpointUrl) {
+
+            var executeQuery = function(sparqlQry) {
+                return $http.get(endpointUrl + '?query=' + encodeURIComponent(sparqlQry) + '&format=json');
+            };
+
+            return {
+                getObjects: function(sparqlQry) {
+                    // Query the endpoint and return a promise of the bindings.
+                    return executeQuery(sparqlQry).then(function(response) {
+                        return response.data.results.bindings;
+                    }, function(response) {
+                        return $q.reject(response.data);
+                    });
+                }
+            };
+        };
+    }
+})();
+
 /* Service for querying a SPARQL endpoint with paging support.
  * Takes the endpoint URL and a mapper object as parameters.
  * The mapper is an object that maps the SPARQL results to objects.
@@ -64,12 +104,6 @@
             }
         };
     }
-})();
-
-(function() {
-    'use strict';
-
-    angular.module('sparql', []);
 })();
 
 (function() {
@@ -476,39 +510,5 @@
                 };
             }
         }
-    }
-})();
-
-/*
-* Service for querying a SPARQL endpoint.
-* Takes the endpoint URL as a parameter.
-*/
-(function() {
-    'use strict';
-
-    /* eslint-disable angular/no-service-method */
-    angular.module('sparql')
-
-    .factory('SparqlService', SparqlService);
-
-    /* ngInject */
-    function SparqlService($http, $q) {
-        return function(endpointUrl) {
-
-            var executeQuery = function(sparqlQry) {
-                return $http.get(endpointUrl + '?query=' + encodeURIComponent(sparqlQry) + '&format=json');
-            };
-
-            return {
-                getObjects: function(sparqlQry) {
-                    // Query the endpoint and return a promise of the bindings.
-                    return executeQuery(sparqlQry).then(function(response) {
-                        return response.data.results.bindings;
-                    }, function(response) {
-                        return $q.reject(response.data);
-                    });
-                }
-            };
-        };
     }
 })();
