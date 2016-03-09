cd5d9201d0615e86078bcbc5ed3539cd89ed1c26
Erkki Heino <erkki.heino@aalto.fi>
2016-03-09 15:56:58 +0200

    Fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,12 @@ module.exports = function(grunt) {
     grunt.initConfig({
         concat: {
             dist: {
-                src: ['src/*.js'],
+                src: ['src/sparql.module.js',
+                        'src/sparql.sparql-service.js',
+                        'src/sparql.advanced-sparql-service.js',
+                        'src/sparql.object-mapper-service.js',
+                        'src/sparql.pager-service.js',
+                        'src/sparql.query-builder-service.js'],
                 dest: 'dist/sparql-service.js'
             }
         }
