994b6dde45e88aaf3d15de235777587ded6ad1b9
Erkki Heino <erkki.heino@aalto.fi>
2016-10-14 16:28:37 +0300

    Add docs folder

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,70 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "Angular SPARQL service with paging and object mapping",
+      "keywords": "adds advancedsparqlservice angular api based construct endpoints injectable mapping maps object objectmapperservice objects overview pageable paging provided queries query querybuilderservice querying returns service services simple simply sparql sparqlservice support"
+    },
+    {
+      "section": "api",
+      "id": "sparql.AdvancedSparqlService",
+      "shortName": "sparql.AdvancedSparqlService",
+      "type": "service",
+      "moduleName": "sparql",
+      "shortDescription": "AdvancedSparqlService",
+      "keywords": "advancedsparqlservice advancedsparqlservicesparqlservice api buildquery config configuration convenience default defines distinct endpoint endpointurl endpoit false functions getobjects getobjectsnogrouping gicen http init instance list makeobjectlist makeobjectlistnogrouping mapped mapper maps method note number object objectmapperservice objects optional order org paged pagerservice pagesize paging parameter placeholder post prefix prefixes promise properties provide qry qryobj query querybuilder querybuilderservice querying querytemplate rdf rdfs required resources result resultpromise resultset resultsetqry return returns select service set size sparql sparqlqry sparqlservice string support truthy uris url usepost var w3"
+    },
+    {
+      "section": "api",
+      "id": "sparql.objectMapperService",
+      "shortName": "sparql.objectMapperService",
+      "type": "service",
+      "moduleName": "sparql",
+      "shortDescription": "objectMapperService",
+      "keywords": "actual api assume candidates defaul discard extended flatten hook implementation inheritance list makeobject makeobjectlist makeobjectlistnogrouping manageable map mapped maps merge merged mergeobjects merges method methods no-op obj object objectmapperservice objects original postprocess processed processing property prototype re-implementation re-implementing result return returned revised reviseobject revising row rows second service single sparql transforming values"
+    },
+    {
+      "section": "api",
+      "id": "sparql.PagerService",
+      "shortName": "sparql.PagerService",
+      "type": "service",
+      "moduleName": "sparql",
+      "shortDescription": "PagerService",
+      "keywords": "advancedsparqlservice api based changes chunks chunksize configured count data default defines distinct function future getallsequentially getmaxpageno getpage getresults gettotalcount init initializes items itemsperpage limit manual method needed notified number objects offset optional paged pageno pagerservice paging parameter placeholder promise queried query receiving recommended removed result resultsetqry returns sequentially service set single size sparql sparqlqry specific subquery total"
+    },
+    {
+      "section": "api",
+      "id": "sparql.QueryBuilderService",
+      "shortName": "sparql.QueryBuilderService",
+      "type": "service",
+      "moduleName": "sparql",
+      "shortDescription": "QueryBuilderService",
+      "keywords": "api assumed bind bound breaks build building buildquery constraints constructed default distinct expression method object optional order org pageable paged paging placeholder prefix prefixes properties qryobj queries query querybuilder querybuilderservice querytemplate rdf rdfs resource resources result resultset resultsetqry returns select service set sparql subquery uris var variable w3"
+    },
+    {
+      "section": "api",
+      "id": "sparql.SparqlService",
+      "shortName": "sparql.SparqlService",
+      "type": "service",
+      "moduleName": "sparql",
+      "shortDescription": "SparqlService",
+      "keywords": "api configuration default endpoint endpointurl endpoit false getobjects http list method object objects optional org parameter post prefix promise properties qry query querying rdfs resultpromise select service sparql sparqlqry sparqlservice string truthy url usepost var w3"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
