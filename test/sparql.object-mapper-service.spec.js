9dee533bbc4aaa3a6b95dc61271d06c47911c79f
Erkki Heino <erkki.heino@aalto.fi>
2017-04-05 17:37:55 +0300

    Merge objects recursively

--- test/sparql.object-mapper-service.spec.js
+++ test/sparql.object-mapper-service.spec.js
@@ -3,10 +3,11 @@
     describe('objectMapperService', function() {
         beforeEach(module('sparql'));
 
-        var objectMapperService;
+        var objectMapperService, _;
 
-        beforeEach(inject(function(_objectMapperService_){
+        beforeEach(inject(function(_objectMapperService_, ___) {
             objectMapperService = _objectMapperService_;
+            _ = ___;
         }));
 
         describe('makeObject', function() {
@@ -42,6 +43,45 @@
         });
 
         describe('mergeObjects', function() {
+            var first, second, third, firstPlace, secondPlace, thirdPlace;
+            beforeEach(function() {
+                firstPlace = {
+                    id: 'http://ldf.fi/warsa/places/municipalities/m_place_1',
+                    info: 'first info',
+                    label: 'Kauhajoki'
+                };
+
+                secondPlace = {
+                    id: 'http://ldf.fi/warsa/places/municipalities/m_place_2',
+                    info: 'second info',
+                    label: 'Kauhajoki'
+                };
+
+                thirdPlace = {
+                    id: 'http://ldf.fi/warsa/places/municipalities/m_place_3',
+                    info: 'third info',
+                    label: 'Kauhajoki'
+                };
+
+                first = {
+                    id: 'http://ldf.fi/warsa/events/event_1',
+                    type: 'http://ldf.fi/warsa/events/event_types/PoliticalActivity',
+                    place: firstPlace
+                };
+
+                second = {
+                    id: 'http://ldf.fi/warsa/events/event_2',
+                    type: 'http://ldf.fi/warsa/events/event_types/PoliticalActivity',
+                    place: secondPlace
+                };
+
+                third = {
+                    id: 'http://ldf.fi/warsa/events/event_3',
+                    type: 'http://ldf.fi/warsa/events/event_types/PoliticalActivity',
+                    place: thirdPlace
+                };
+            });
+
             it('merges two objects', function() {
                 var first = {
                     id: 'http://ldf.fi/warsa/events/event_1',
@@ -136,6 +176,182 @@
                 };
 
                 var merged = objectMapperService.mergeObjects(first, second);
+
+                expect(merged).toEqual(expected);
+            });
+
+            it('merges two object values recursively', function() {
+                second.id = first.id;
+                secondPlace.id = firstPlace.id;
+
+                var expectedPlace = {
+                    id: firstPlace.id,
+                    info: [firstPlace.info, secondPlace.info],
+                    label: firstPlace.label
+                };
+
+                var expected = {
+                    id: first.id,
+                    type: first.type,
+                    place: expectedPlace
+                };
+
+                var merged = objectMapperService.mergeObjects(_.cloneDeep(first), _.cloneDeep(second));
+
+                expect(merged).toEqual(expected);
+            });
+
+            it('merges object values recursively', function() {
+                third.id = first.id;
+
+                firstPlace.info = ['first info', 'second info'];
+                thirdPlace.id = firstPlace.id;
+
+                var expectedPlace = {
+                    id: firstPlace.id,
+                    info: ['first info', 'second info', 'third info'],
+                    label: firstPlace.label
+                };
+
+                var expected = {
+                    id: first.id,
+                    type: first.type,
+                    place: expectedPlace
+                };
+
+                var merged = objectMapperService.mergeObjects(_.cloneDeep(first), _.cloneDeep(third));
+
+                expect(merged).toEqual(expected);
+            });
+
+            it('merges values that are lists', function() {
+                second.id = first.id;
+
+                first.place = [firstPlace, secondPlace];
+                second.place = [thirdPlace];
+
+                var expected = {
+                    id: first.id,
+                    type: first.type,
+                    place: [firstPlace, secondPlace, thirdPlace]
+                };
+
+                var merged = objectMapperService.mergeObjects(_.cloneDeep(first), _.cloneDeep(second));
+
+                expect(merged).toEqual(expected);
+            });
+
+            it('removes duplicates when merging lists', function() {
+                second.id = first.id;
+
+                first.place = [firstPlace, secondPlace];
+                second.place = [secondPlace, thirdPlace];
+
+                var expected = {
+                    id: first.id,
+                    type: first.type,
+                    place: [firstPlace, secondPlace, thirdPlace]
+                };
+
+                var merged = objectMapperService.mergeObjects(_.cloneDeep(first), _.cloneDeep(second));
+
+                expect(merged).toEqual(expected);
+            });
+
+            it('works recursively with lists', function() {
+                second.id = first.id;
+                secondPlace.id = firstPlace.id;
+                firstPlace.info = ['first info', 'second info'];
+                secondPlace.info = ['second info', 'third info'];
+
+                var expectedPlace = {
+                    id: firstPlace.id,
+                    info: ['first info', 'second info', 'third info'],
+                    label: firstPlace.label
+                };
+
+                var expected = {
+                    id: first.id,
+                    type: first.type,
+                    place: expectedPlace
+                };
+
+                var merged = objectMapperService.mergeObjects(_.cloneDeep(first), _.cloneDeep(second));
+
+                expect(merged).toEqual(expected);
+            });
+        });
+
+        describe('mergeObjectToList', function() {
+            var firstPlace, secondPlace, thirdPlace, first, second, third;
+            beforeEach(function() {
+                firstPlace = {
+                    id: 'http://ldf.fi/warsa/places/municipalities/m_place_119',
+                    info: 'first info',
+                    label: 'Kauhajoki'
+                };
+
+                secondPlace = {
+                    id: 'http://ldf.fi/warsa/places/municipalities/m_place_119',
+                    info: 'second info',
+                    label: 'Kauhajoki'
+                };
+
+                thirdPlace = {
+                    id: 'http://ldf.fi/warsa/places/municipalities/m_place_119',
+                    info: 'third info',
+                    label: 'Kauhajoki'
+                };
+                first = {
+                    id: 'http://ldf.fi/warsa/events/event_1',
+                    type: 'http://ldf.fi/warsa/events/event_types/PoliticalActivity',
+                    place: firstPlace
+                };
+
+                second = {
+                    id: 'http://ldf.fi/warsa/events/event_2',
+                    type: 'http://ldf.fi/warsa/events/event_types/PoliticalActivity',
+                    place: secondPlace
+                };
+
+                third = {
+                    id: 'http://ldf.fi/warsa/events/event_2',
+                    type: 'http://ldf.fi/warsa/events/event_types/PoliticalActivity',
+                    place: thirdPlace
+                };
+            });
+
+            it('merges an object with another object in the list if they have the same id', function() {
+
+                var expectedPlace = {
+                    id: thirdPlace.id,
+                    info: ['second info', 'third info'],
+                    label: thirdPlace.label
+                };
+
+                var expected = [
+                    first,
+                    {
+                        id: second.id,
+                        type: second.type,
+                        place: expectedPlace
+                    }
+                ];
+
+                var merged = objectMapperService.mergeObjectToList([_.cloneDeep(first),
+                    _.cloneDeep(second)], _.cloneDeep(third));
+
+                expect(merged).toEqual(expected);
+            });
+
+            it('appends the object to the list if no other object with the same id is found', function() {
+
+                third.id = 'http://ldf.fi/warsa/events/event_3';
+
+                var expected = [first, second, third];
+
+                var merged = objectMapperService.mergeObjectToList([_.cloneDeep(first),
+                    _.cloneDeep(second)], _.cloneDeep(third));
 
                 expect(merged).toEqual(expected);
             });
